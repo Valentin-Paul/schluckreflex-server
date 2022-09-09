2edac5f07333ec04e752c8b4a09d312e2fd130f1
Valentin Paul <valentin@vamptech.de>
2022-09-09 11:11:48 +0200

    created recipe model

---
+++ models/Recipe.model.js
@@ -0,0 +1,22 @@
+const {Schema, model} = require("mongoose");
+
+const recipeSchema = new Schema(
+    {
+        recipeName:String,
+        ingredientes:[[
+            // {
+            // ingrediente: String,
+            // amount: String
+            // }
+        ]],
+        description: String,    
+        tags:[String]
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Recipe = model("Recipe", recipeSchema);
+
+module.exports = Recipe;
